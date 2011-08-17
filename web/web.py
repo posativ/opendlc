f4e8a5593fe2822339dbe47f8f9bfb9d78375309
posativ <info@posativ.org>
2011-08-17 22:42:15 +0200

    python2.6 compatibility, some doc

--- web/web.py
+++ web/web.py
@@ -3,23 +3,35 @@
 #
 # Copyright 2011 posativ <info@posativ.org>. All rights reserved.
 # License: BSD Style, 2 clauses
+#
+# web.py is a webserver using bottle.py. It is intended, to use another
+# webserver as proxy for this. lighttpd setup (using mod_proxy):
+# $HTTP["url"] =~ "/decrypt/" {
+#     proxy.server = ("" =>
+#         (("host" => "127.0.0.1", "port" => 8000)))
+# 
+# }
 
 import sys
 sys.path.append('../')
 
-import bottle
 from bottle import route, run, post, request, response
 
-bottle.debug(True)
+#import bottle
+#bottle.debug(True)
 
 from decrypt import rsdf, dlc, ccf
 from xml.etree.ElementTree import fromstring
 
+# the template file
 tt = open('main.html').read()
-key, iv, name = [k.strip() for k in open('../keys').read().split(':')]
+
+# key file's layout: `APPKEY:IV:APPNAME` -> e.g. `acf...23f:2c...fd:jdsuck`
+key, iv, name = [k.strip() for k in open('../keys').readline().split(':')]
 
 @post('/decrypt/:container')
 def decrypt(container=None):
+    '''handles every supported decrypt/(.+) container'''
     
     response.headers['Content-Type'] = 'text/plain'
     
@@ -27,34 +39,36 @@ def decrypt(container=None):
     if data != None:
         data.file.seek(0, 2)
         size = data.file.tell()
-        if size > 2**20: # 1 MiB
+        if size > 2**20: # 1 MiB file limit
             return ''
         data.file.seek(0)
         data = data.file.read()
     else:
         data = request.forms.get('data', '')
-    
     if container == 'rsdf':
         return '\n'.join(rsdf.decrypt(data))
+        
     elif container == 'dlc':
         try:
             data = dlc.decrypt(data, key, iv, name)
         except TypeError:
             return 'unable to decrypt DLC'
         el = fromstring(''.join(dlc.Debase(data).result))
-        return '\n'.join([item.find('url').text for item in el.iter('file')])
+        return '\n'.join([item.find('url').text for item in el.getiterator('file')])
     elif container == 'ccf':
         try:
             data = ccf.decrypt(data)
         except:
+            # we fully rely on service.jdownloader.org, everything can happen...
             return 'service failure'
         data = dlc.decrypt(data, key, iv, name)
         el = fromstring(''.join(dlc.Debase(data).result))
-        return '\n'.join([item.find('url').text for item in el.iter('file')])
+        return '\n'.join([item.find('url').text for item in el.getiterator('file')])
 
 @route('/decrypt/')
 def index():
-    #tt = Template(open('main.html').read())
     return tt
-
-run(host='localhost', port=8000)
+    
+if __name__ == '__main__':
+    # starts bottle
+    run(host='localhost', port=8000)
