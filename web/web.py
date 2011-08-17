7a03d61e4e58d154fcae9d3a1e4aeb19f28c1487
posativ <info@posativ.org>
2011-08-17 22:22:12 +0200

    added CCF support and file upload

--- web/web.py
+++ web/web.py
@@ -9,28 +9,46 @@ sys.path.append('../')
 
 import bottle
 from bottle import route, run, post, request, response
-#from jinja2 import Template
 
-#bottle.debug(True)
+bottle.debug(True)
 
-import rsdf, dlc
+from decrypt import rsdf, dlc, ccf
 from xml.etree.ElementTree import fromstring
 
 tt = open('main.html').read()
+key, iv, name = [k.strip() for k in open('../keys').read().split(':')]
 
 @post('/decrypt/:container')
 def decrypt(container=None):
     
     response.headers['Content-Type'] = 'text/plain'
     
-    data = request.forms.get('data')
+    data = request.files.get('file', None)
+    if data != None:
+        data.file.seek(0, 2)
+        size = data.file.tell()
+        if size > 2**20: # 1 MiB
+            return ''
+        data.file.seek(0)
+        data = data.file.read()
+    else:
+        data = request.forms.get('data', '')
+    
     if container == 'rsdf':
         return '\n'.join(rsdf.decrypt(data))
     elif container == 'dlc':
         try:
-            data = dlc.decrypt(data)
+            data = dlc.decrypt(data, key, iv, name)
         except TypeError:
             return 'unable to decrypt DLC'
+        el = fromstring(''.join(dlc.Debase(data).result))
+        return '\n'.join([item.find('url').text for item in el.iter('file')])
+    elif container == 'ccf':
+        try:
+            data = ccf.decrypt(data)
+        except:
+            return 'service failure'
+        data = dlc.decrypt(data, key, iv, name)
         el = fromstring(''.join(dlc.Debase(data).result))
         return '\n'.join([item.find('url').text for item in el.iter('file')])
 
